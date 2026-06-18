3360120d68da309a1534be5934174772d0243975
LKJ_MC <LK_IQBoost@163.com>
2026-06-18 13:15:19 +0800

    更新于 周四 2026/06/18 13:15:19.49

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ class MyPlugin(Star):
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        pass
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("zzz")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    async def zzz(self, event: AstrMessageEvent):
         user_name = event.get_sender_name()
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        message_chain = event.get_messages()
         logger.info(message_chain)
-        yield event.plain_result(f"你好, {user_name}, 绝区零登顶！") # 发送一条纯文本消息
+        yield event.plain_result(f"你好, {user_name}, 绝区零登顶！")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        pass
