6e96fa46903e6f8c7ae2ca13cfd4b5a6b58da40a
LKJ_MC <LK_IQBoost@163.com>
2026-06-19 15:13:25 +0800

    更新于 周五 2026/06/19 15:13:25.39

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.api.message_components import Plain
 from astrbot.api import logger
-import astrbot.api.message_components as Comp
+from astrbot.core.message.message_event_result import MessageChain
+from astrbot.api.message_components import Node, Plain, Image
 
 @register("Zenless Zone Zore", "LK", "一个简单的 Fairy 插件", "1.0.0")
 class FairyPlugin(Star):
@@ -19,32 +21,22 @@ class FairyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"你好, {user_name}, 绝区零登顶！")
 
-    @filter.command("image") # 注册一个 /image 指令，接收 text 参数。
-    async def on_aiocqhttp(self, event: AstrMessageEvent, text: str):
-        url = await self.text_to_image(text) # text_to_image() 是 Star 类的一个方法。
-        # path = await self.text_to_image(text, return_url = False) # 如果你想保存图片到本地
-        yield event.image_result(url)
+    @filter.command("getsauth")
+    async def getsauth(self, event: AstrMessageEvent, num: int):
+        yield event.plain_result("发送中...")
+        target_umo = "aiocqhttp:private:3931028976"
+        for _ in range(num):
+            await self.context.send_message(
+                target_umo, MessageChain([Plain("/generate")])
+            )
 
-    @filter.command("get")
-    async def get(self, event: AstrMessageEvent, text: str):
-        chain = [
-            Comp.At(qq=event.get_sender_id()),  # At 消息发送者
-            Comp.Plain("年度校草写真："),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\1.png"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\2.png"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\3.png"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\4.jpg"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\5.png"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\6.jpg"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\7.png"),
-            Comp.Image.fromFileSystem(r"D:\Project\AstrBot\data\plugins\astrbot_plugin_fairy\data\8.png"),
-            Comp.Plain("已严肃观看"),
-        ]
-        yield event.chain_result(chain)
+    @filter.command("image") 
+    async def on_aiocqhttp(self, event: AstrMessageEvent, text: str):
+        url = await self.text_to_image(text)
+        yield event.image_result(url)
 
     @filter.command("fakechat")
     async def fakechat(self, event: AstrMessageEvent, uin: int, name: str, text: str):
-        from astrbot.api.message_components import Node, Plain, Image
         node = Node(
             uin=uin, name=name, content=[Plain(text)]
         )
